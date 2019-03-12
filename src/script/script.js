4f8439c5be6634bd481b0a4b5ec9d64589b9da3e
krzszk96 <krzszk96@gmail.com>
2019-03-12 13:33:11 +0100

    working calculate to any date

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,4 @@
-function passDate(){
-  var importdate = document.getElementById("putdate").value;
-
+function today(){
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth()+1; //January is 0!
@@ -10,11 +8,27 @@ function passDate(){
   if(mm<10) {mm = '0'+mm}
 
   today = yyyy + '-' + mm + '-' + dd;
+  return today;
+}
+
+function passDate(){
+  var importdate = document.getElementById("putdate").value;
 
   var startDate = Date.parse(importdate);
-  var endDate = Date.parse(today);
+  var endDate = Date.parse(today());
   var timeDiff = endDate - startDate;
   daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
-  document.getElementById("daysto").innerHTML = daysDiff;
+  return daysDiff;
+}
+
+function createEvent(){
+  var div = document.createElement('div');
+  div.innerHTML = document.getElementById("putdate").value + " days since: " + passDate();
+  div.setAttribute('class', 'myclass'); // and make sure myclass has some styles in css
+  document.getElementById("container").appendChild(div);
+}
+function btn(){
+  passDate()
+  createEvent()
 }
