09c87f28b26c8c87028151412c9d4e47055c7dd3
unknown <krzszk96@gmail.com>
2019-04-09 22:44:22 +0200

    added warehouse and harvester

--- src/script/script.js
+++ src/script/script.js
@@ -1,18 +1,23 @@
 var logs = 0;
 var boards = 0;
-var money = 50000;
+var money = 0;
 var chainsaw = 0;
 var log_worker = 0;
 var cut_worker = 0;
 var executed_log_worker = false;
 var executed_cut_worker = false;
 var chopbar = 0;
+var warehouse_space = 100;
+var harvesters = 0;
 
 function chopTree(){
-  if(chainsaw==0){
-    logs++;
-  }else{
-    logs = logs + 2;
+  if(warehouse_space>logs){
+    if(chainsaw==1){logs = logs + 2;
+    }else if (harvesters>0) {
+      logs = logs + 10*harvesters;
+    }else {
+      logs++;
+    }
   }
   document.getElementById('logs').innerHTML = 'Logs: ' + logs;
 }
@@ -21,30 +26,33 @@ function chopBar(){
   switch(chopbar) {
     case 0:
       document.getElementById("chopbar").style.width = "30px";
+      document.getElementById("axe_img").style.transform = "rotate(20deg)";
       chopbar++;
     break;
 
     case 1:
       document.getElementById("chopbar").style.width = "60px";
+      document.getElementById("axe_img").style.transform = "rotate(40deg)";
       chopbar++;
     break;
 
     case 2:
       document.getElementById("chopbar").style.width = "90px";
+      document.getElementById("axe_img").style.transform = "rotate(60deg)";
       chopbar = 0;
       chopTree();
     break;
   }
 }
 
-function log_worker_calc(){
+function logWorkerCalc(){
     window.setInterval( function(){
     logs = logs + log_worker;
     document.getElementById('logs').innerHTML = 'Logs: ' + logs;
   }, 5000);
 }
 
-function cut_worker_calc(){
+function cutWorkerCalc(){
     window.setInterval( function(){
     if(logs>0){
       if(logs<cut_worker){
@@ -78,8 +86,6 @@ function sellBoards(){
   }
 }
 
-
-
 document.getElementById("menu").addEventListener("click", function(){
 
   if(event.target.id=='chainsaw'){
@@ -96,7 +102,7 @@ document.getElementById("menu").addEventListener("click", function(){
       log_worker++;
         if (!executed_log_worker) {
           executed_log_worker = true;
-          log_worker_calc();
+          logWorkerCalc();
         }
     }
     document.getElementById('disp_log_workers').innerHTML = 'Number of log workers: ' + log_worker;
@@ -108,10 +114,26 @@ document.getElementById("menu").addEventListener("click", function(){
       cut_worker++;
         if (!executed_cut_worker) {
           executed_cut_worker = true;
-          cut_worker_calc();
+          cutWorkerCalc();
         }
     }
     document.getElementById('disp_cut_workers').innerHTML = 'Number of cut workers: ' + cut_worker;
+    document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
+  }
+  if(event.target.id=='buy_warehouse_space'){
+    if(money>=10000){
+      money = money - 10000;
+      warehouse_space = warehouse_space + 100;
+    }
+    document.getElementById('warehouse_space').innerHTML = 'Warehouse space: ' + warehouse_space;
+    document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
+  }
+  if(event.target.id=='buy_harvester'){
+    if(money>=100000){
+      money = money - 100000;
+      harvesters++;
+    }
+    document.getElementById('disp_harvesters').innerHTML = 'Number of harvesters: ' + harvesters;
     document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
   }
 });
