90af8cdaacf1ab14bcfe211aa71f9cd0df1cf60a
unknown <krzszk96@gmail.com>
2019-04-24 00:30:04 +0200

    stats to obj

--- src/script/script.js
+++ src/script/script.js
@@ -1,30 +1,36 @@
 //git subtree split --branch gh-pages --prefix src/
 //git push -u origin gh-pages
-var logs = 0;
-var boards = 0;
-var money = 0;
-var chainsaw = 0;
-var log_worker = 0;
-var cut_worker = 0;
-var executed_log_worker = false;
-var executed_cut_worker = false;
+// create eventlistener that will catch every click on the web page, and save obj stats.
+//then find a way that update ojbect after every click
+
 var chopbar = 0;
-var warehouse_space = 100;
-var harvesters = 0;
-var sawmill_level = 0;
-var sawmill_mult = 5000;
+
+var Stats = {
+  logs: 0,
+  boards: 0,
+  money: 0,
+  chainsaw: 0,
+  log_worker: 0,
+  cut_worker: 0,
+  executed_log_worker: false,
+  executed_cut_worker: false,
+  warehouse_space: 100,
+  harvesters: 0,
+  sawmill_level: 0,
+  sawmill_mult: 5000
+}
 
 function chopTree(){
-  if(warehouse_space>logs){
-    if(harvesters>0){
-      logs = logs + 10*harvesters;
-    }else if (chainsaw==1) {
-      logs = logs + 2;
+  if(Stats.warehouse_space>Stats.logs){
+    if(Stats.harvesters>0){
+      Stats.logs = Stats.logs + 10*Stats.harvesters;
+    }else if (Stats.chainsaw==1) {
+      Stats.logs = Stats.logs + 2;
     }else {
-      logs++;
+      Stats.logs++;
     }
   }
-  document.getElementById('logs').innerHTML = 'Logs: ' + logs;
+  document.getElementById('logs').innerHTML = 'Logs: ' + Stats.logs;
 }
 
 function chopBar(){
@@ -52,116 +58,117 @@ function chopBar(){
 
 function logWorkerCalc(){
     window.setInterval( function(){
-    if(warehouse_space>logs){
-      var temp = logs + log_worker;
-      if(temp>warehouse_space){
-        logs = logs + warehouse_space-logs;
+    if(Stats.warehouse_space>Stats.logs){
+      var temp = Stats.logs + Stats.log_worker;
+      if(temp>Stats.warehouse_space){
+        Stats.logs = Stats.logs + Stats.warehouse_space - Stats.logs;
       }else {
-        logs = logs + log_worker;
+        Stats.logs = Stats.logs + Stats.log_worker;
       }
 
     }
-    document.getElementById('logs').innerHTML = 'Logs: ' + logs;
+    document.getElementById('logs').innerHTML = 'Logs: ' + Stats.logs;
   }, 5000);
 }
 
 function cutWorkerCalc(){
     window.setInterval( function(){
-      if(logs<cut_worker){
-        boards = boards + logs * 4;
-        logs = 0;
+      if(Stats.logs<Stats.cut_worker){
+        Stats.boards = Stats.boards + Stats.logs * 4;
+        Stats.logs = 0;
       }else{
-        if(sawmill_level>0){
-          logs = logs - cut_worker;
-          boards = boards + cut_worker * 4 * sawmill_level+1;
+        if(Stats.sawmill_level>0){
+          Stats.logs = Stats.logs - Stats.cut_worker;
+          Stats.boards = Stats.boards + Stats.cut_worker * 4 * Stats.sawmill_level+1;
         }else{
-          logs = logs - cut_worker;
-          boards = boards + cut_worker * 4;
+          Stats.logs = Stats.logs - Stats.cut_worker;
+          Stats.boards = Stats.boards + Stats.cut_worker * 4;
          }
       }
-      document.getElementById('boards').innerHTML = 'Boards: ' + boards;
-      document.getElementById('logs').innerHTML = 'Logs: ' + logs;
+      document.getElementById('boards').innerHTML = 'Boards: ' + Stats.boards;
+      document.getElementById('logs').innerHTML = 'Logs: ' + Stats.logs;
 
   }, 5000);
 }
 
 function cutBoards(){
-  if(logs>0){
-    logs--;
-    boards=boards+4;
-    document.getElementById('boards').innerHTML = 'Boards: ' + boards;
-    document.getElementById('logs').innerHTML = 'Logs: ' + logs;
+
+  if(Stats.logs>0){
+    Stats.logs--;
+    Stats.boards = Stats.boards + 4;
   }
+  document.getElementById('boards').innerHTML = 'Boards: ' + Stats.boards;
+  document.getElementById('logs').innerHTML = 'Logs: ' + Stats.logs;
 }
 
 function sellBoards(){
-  if(boards>0){
-    money = money + boards * 4;
-    boards = 0;
-    document.getElementById('boards').innerHTML = 'Boards: ' + boards;
-    document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
+  if(Stats.boards>0){
+    Stats.money = Stats.money + Stats.boards * 4;
+    Stats.boards = 0;
+    document.getElementById('boards').innerHTML = 'Boards: ' + Stats.boards;
+    document.getElementById('money').innerHTML = 'Money: ' + Stats.money + ' $';
   }
 }
 
 document.getElementById("menu1").addEventListener("click", function(){
 
   if(event.target.id=='chainsaw'){
-    if(money>=200){
-      money = money - 200;
-      chainsaw = 1;
+    if(Stats.money>=200){
+      Stats.money = Stats.money - 200;
+      Stats.chainsaw = 1;
       document.getElementById("chainsaw").style.display = "none";
-      document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
+      document.getElementById('money').innerHTML = 'Money: ' + Stats.money + ' $';
     }
   }
   if(event.target.id=='log_worker'){
-    if(money>=1000){
-      money = money - 1000;
-      log_worker++;
-        if (!executed_log_worker) {
-          executed_log_worker = true;
+    if(Stats.money>=1000){
+      Stats.money = Stats.money - 1000;
+      Stats.log_worker++;
+        if (!Stats.executed_log_worker) {
+          Stats.executed_log_worker = true;
           logWorkerCalc();
         }
     }
-    document.getElementById('disp_log_workers').innerHTML = 'Number of log workers: ' + log_worker;
-    document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
+    document.getElementById('disp_log_workers').innerHTML = 'Number of log workers: ' + Stats.log_worker;
+    document.getElementById('money').innerHTML = 'Money: ' + Stats.money + ' $';
   }
   if(event.target.id=='cut_worker'){
-    if(money>=1500){
-      money = money - 1500;
-      cut_worker++;
-        if (!executed_cut_worker) {
-          executed_cut_worker = true;
+    if(Stats.money>=1500){
+      Stats.money = Stats.money - 1500;
+      Stats.cut_worker++;
+        if (!Stats.executed_cut_worker) {
+          Stats.executed_cut_worker = true;
           cutWorkerCalc();
         }
     }
-    document.getElementById('disp_cut_workers').innerHTML = 'Number of cut workers: ' + cut_worker;
-    document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
+    document.getElementById('disp_cut_workers').innerHTML = 'Number of cut workers: ' + Stats.cut_worker;
+    document.getElementById('money').innerHTML = 'Money: ' + Stats.money + ' $';
   }
   if(event.target.id=='buy_warehouse_space'){
-    if(money>=10000){
-      money = money - 10000;
-      warehouse_space = warehouse_space + 100;
+    if(Stats.money>=10000){
+      Stats.money = Stats.money - 10000;
+      Stats.warehouse_space = Stats.warehouse_space + 100;
     }
-    document.getElementById('warehouse_space').innerHTML = 'Warehouse space: ' + warehouse_space;
-    document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
+    document.getElementById('warehouse_space').innerHTML = 'Warehouse space: ' + Stats.warehouse_space;
+    document.getElementById('money').innerHTML = 'Money: ' + Stats.money + ' $';
   }
   if(event.target.id=='buy_harvester'){
-    if(money>=100000){
-      money = money - 100000;
-      harvesters++;
+    if(Stats.money>=100000){
+      Stats.money = Stats.money - 100000;
+      Stats.harvesters++;
     }
-    document.getElementById('disp_harvesters').innerHTML = 'Number of harvesters: ' + harvesters;
-    document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
+    document.getElementById('disp_harvesters').innerHTML = 'Number of harvesters: ' + Stats.harvesters;
+    document.getElementById('money').innerHTML = 'Money: ' + Stats.money + ' $';
   }
   if(event.target.id=='sawmill'){
-    if(money>=1000000){
-      money = money - 1000000;
-      sawmill_level++;
+    if(Stats.money>=1000000){
+      Stats.money = Stats.money - 1000000;
+      Stats.sawmill_level++;
       document.getElementById("sawmill").style.display = "none";
       document.getElementById("cut").style.display = "none";
       document.getElementById("sell").className = "sellwhenmill";
-      document.getElementById('money').innerHTML = 'Money: ' + money + ' $';
-      document.getElementById('sawmill_info').innerHTML = 'Sawmill level: ' + sawmill_level;
+      document.getElementById('money').innerHTML = 'Money: ' + Stats.money + ' $';
+      document.getElementById('sawmill_info').innerHTML = 'Sawmill level: ' + Stats.sawmill_level;
     }
   }
 });
